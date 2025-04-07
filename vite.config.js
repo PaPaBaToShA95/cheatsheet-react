f8ad7d39b2499654d3cb97e34b7d13e025dabc1f
oleksijermantraut <mawabek43@gmail.com>
2025-04-07 15:03:36 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/cheatsheet-react/",
   server: {
     host: true, // або '0.0.0.0' — щоб слухати всі мережі
     port: 5173, // або інший порт
